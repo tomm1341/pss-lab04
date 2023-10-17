840110cd1ac1b6eb23869864e2045ef3cc501439
tomm1341 <tommaso.foca@studio.unibo.it>
2023-10-17 10:23:08 +0200

    Phase 2 commit 2

--- 01-encapsulation/src/main/java/it/unibo/encapsulation/SmartLamp.java
+++ 01-encapsulation/src/main/java/it/unibo/encapsulation/SmartLamp.java
@@ -10,13 +10,13 @@ public class SmartLamp {
 
 
     public SmartLamp(final boolean lampOn, final int lightIntensity, final String lightColor, final String brand, final String model){
-
         this.lampOn = lampOn;
         this.lightColor = lightColor;
         this.lightIntensity = lightIntensity;
         this.brand = brand;
         this.model = model;
     }
+
     public boolean getOnOff(){
         return lampOn;
     }
@@ -37,6 +37,12 @@ public class SmartLamp {
         return model;
     }
 
-    
+    public void lampInfo(){
+        System.out.println("Smart Lamp On: " + this.lampOn);
+        System.out.println("Smart Lamp Intensity: " + this.lightIntensity);
+        System.out.println("Smart Lamp Color: " + this.lightColor);
+        System.out.println("Smart Lamp Brand: " + this.brand);
+        System.out.println("Smart Lamp Model: " + this.model);
+    }
 
 }
