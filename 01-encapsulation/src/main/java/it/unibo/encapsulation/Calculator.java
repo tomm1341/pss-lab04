87057939f38bc06ad635977e0638638854d7b48a
tomm1341 <tommaso.foca@studio.unibo.it>
2023-10-17 09:58:07 +0200

    Phase 1 + Calculator

--- 01-encapsulation/src/main/java/it/unibo/encapsulation/Calculator.java
+++ 01-encapsulation/src/main/java/it/unibo/encapsulation/Calculator.java
@@ -2,9 +2,17 @@ package it.unibo.encapsulation;
 
 public class Calculator {
 
-    int operationsPerformed;
-    double lastResult;
+   private int operationsPerformed;
+   private double lastResult;
 
+   public int getOperationsPerformed(){
+    return operationsPerformed;
+   }
+
+   public double getLastResult(){
+    return lastResult;
+   }
+   
     public Calculator() {
         this.operationsPerformed = 0;
         this.lastResult = 0;
